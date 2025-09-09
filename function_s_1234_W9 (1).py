22228425dff10fe453a859a7bd296bd947989459
thunchanokpr68-boop <thunchanok.pr.68@ubu.ac.th>
2025-09-09 22:24:06 +0700

    Add files via upload

---
+++ function_s_1234_W9 (1).py
@@ -0,0 +1,19 @@
+def greet_user(name):
+greet_user("Somsak")
+  print(f"สวัสดี ,Somsak!")
+  
+def calculate_rectangle_area(width, height):
+   return width * height
+area = calculate_rectangle_area(width=8, height=8)
+print(f" ค่าพื้นที่ : {area}")
+
+def def is_palindrome(word):
+  pass
+def find_max(numbers):
+  pass
+def power(base, exponent=2):
+  pass
+def factorial(n):
+  pass
+def print_base6(n):
+  pass
